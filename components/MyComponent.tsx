ab7fee2efef4c850305a7c578656d327d4d7524f
Oliver Joseph Ash <oliverjash@gmail.com>
2022-01-02 15:02:07 +0000

    Format

--- components/MyComponent.tsx
+++ components/MyComponent.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 export const MyComponent = () => {
-    const [counter, setCounter] = React.useState(0);
-    const onClick = React.useCallback(() => {
-        setCounter((n) => n + 1);
-    }, []);
-    return <button onClick={onClick}>{counter}</button>;
+  const [counter, setCounter] = React.useState(0);
+  const onClick = React.useCallback(() => {
+    setCounter((n) => n + 1);
+  }, []);
+  return <button onClick={onClick}>{counter}</button>;
 };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withMDX = require("@next/mdx")({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-    },
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 });
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
-    reactStrictMode: true,
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 });
