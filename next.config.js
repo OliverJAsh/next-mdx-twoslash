fe5b71bab6dc9a7774604503d4fcd448c72db0e2
Oliver Joseph Ash <oliverjash@gmail.com>
2022-01-02 12:40:05 +0000

    mdx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+});
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+module.exports = withMDX({
+    reactStrictMode: true,
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+});
